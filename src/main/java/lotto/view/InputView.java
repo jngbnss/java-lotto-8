ac3112d92cf2850d5268c32a8ff7b351c81a2ab8
jngbnss <jngbnmn@naver.com>
2025-10-31 15:57:52 +0900

    test: LottoChecker test  정답 및 등수 확인

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,7 +7,9 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public int inputPurchaseAmount() {
-            System.out.println("구입금액을 입력해 주세요.");
+//            System.out.println("구입금액을 입력해 주세요.");
+//            return = Integer.parseInt(readLine());
+
             try {
                 int amount = Integer.parseInt(readLine());
                 validateAmount(amount);
